7b78046f04eb8eaafc7a5c53a7fe4e0c8427bdf2
gpmike31 <gpmike3105@gmail.com>
2022-04-25 17:32:26 -0500

    finish updating category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,21 +13,53 @@ router.get('/', async (req, res) => {
     res.json(categories)
   }
   catch (err) {
-    res.status(3001).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categories = await Category.findOne({ 
+      where: 
+      {id: req.params.id},
+      include: [Product]
+  })
+
+  res.json(categories)
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categories = await Category.create(req.body);
+
+    res.json(categories);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const categories = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    } );
+
+    
+  }
+  catch(err) {
+
+  }
 });
 
 router.delete('/:id', (req, res) => {
